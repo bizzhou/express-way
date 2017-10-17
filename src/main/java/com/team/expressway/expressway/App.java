64991be9c709e71a1b237efbc3797d96188e68c4
melanie0926 <melanieeelin@gmail.com>
2017-10-17 13:38:46 -0400

    setup maven

---
+++ src/main/java/com/team/expressway/expressway/App.java
@@ -0,0 +1,13 @@
+package com.team.expressway.expressway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
