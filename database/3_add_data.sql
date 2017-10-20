da5b3b66f188be1ca9abac9ea9a6ee66cf991664
bizzhou <bin.zhou.1@stonybrook.edu>
2017-10-20 13:17:26 -0400

    .

--- database/3_add_data.sql
+++ database/3_add_data.sql
@@ -30,7 +30,8 @@ INSERT INTO `Flight` (flight_number, date_of_week, seating_capacity, airline, mi
   ('111', '1111111', '150', 'JA', 2, 2),
   ('1337', '0000011', '33', 'AM', 2, 2);
 
-INSERT INTO `Legs` (airline_id, flight_number, leg_number, from_airport, to_airport, departure_time, arrival_time) VALUES
+INSERT INTO `Legs` (airline_id, flight_number, leg_number, from_airport, to_airport, departure_time, arrival_time)
+VALUES
   ('AA', '111', 1, 'JFK', 'LGA', '2011-01-05 09:00:00', '2011-01-05 11:00:00'),
   ('AA', '111', 2, 'LGA', 'LAX', '2011-01-05 17:00:00', '2011-01-05 19:00:00'),
   ('AA', '111', 3, 'LAX', 'NRT', '2011-01-06 07:30:00', '2011-01-06 10:00:00'),
@@ -40,33 +41,57 @@ INSERT INTO `Legs` (airline_id, flight_number, leg_number, from_airport, to_airp
   ('AM', '1337', 1, 'SFO', 'JFK', '2011-01-13 05:00:00', '2011-01-13 07:00:00'),
   ('AM', '1337', 2, 'JFK', 'TNR', '2011-01-13 23:00:00', '2011-01-14 03:00:00');
 
-INSERT INTO `Person` (id, first_name, last_name, address, city, state, zip_code) VALUES
-  (1 , 'Jane', 'Smith', '100 Nicolls Rd', 'Stony Brook', 'NY', '117790'),
-  (2 , 'John', 'Doe', '123 N Fake Street', 'New York', 'NY', '10001'),
-  (3 , 'Rick', 'Astley', '1337 Internet Lane', 'Los Angeles', 'CA', '90001');
+
+INSERT INTO express_way.Person (id, first_name, last_name, address, city, state, zip_code)
+VALUES (1, 'Jane', 'Smith', '100 Nicolls Rd', 'Stony Brook', 'NY', 117790);
+INSERT INTO express_way.Person (id, first_name, last_name, address, city, state, zip_code)
+VALUES (2, 'John', 'Doe', '123 N Fake Street', 'New York', 'NY', 10001);
+INSERT INTO express_way.Person (id, first_name, last_name, address, city, state, zip_code)
+VALUES (3, 'Rick', 'Astley', '1337 Internet Lane', 'Los Angeles', 'CA', 90001);
+INSERT INTO express_way.Person (id, first_name, last_name, address, city, state, zip_code)
+VALUES (4, 'Mark', 'Edison', '2333 Hacker Lane', 'Los Angeles', 'CA', 90001);
+INSERT INTO express_way.Person (id, first_name, last_name, address, city, state, zip_code)
+VALUES (5, 'Peter', 'Vim', '6666 Hacker Lane', 'Los Angeles', 'CA', 90001);
+
 
 INSERT INTO Customer (id, account_number) VALUES
   (1, '1000001'),
   (2, '1000002'),
   (3, '1000003');
 
-INSERT INTO Passengers (id, account_number ,telephone, email) VALUES
-  (1 , '1000001' ,'555-555-5555', 'awesomejane@ftw.com'),
-  (2,'1000002' ,'123-123-1234', 'jdoe@woot.com'),
+INSERT INTO Passengers (id, account_number, telephone, email) VALUES
+  (1, '1000001', '555-555-5555', 'awesomejane@ftw.com'),
+  (2, '1000002', '123-123-1234', 'jdoe@woot.com'),
   (3, '1000003', '314-159-2653', 'rickroller@rolld.com');
 
-INSERT INTO `Reservations` (reservation_number, account_number, booking_fee, total_fare, reservation_date) VALUES
-  ('111', '1000001' , '20.00' ,'1200', '2011-01-05 09:00:00'),
-  ('222','1000002', '20.00' ,'500', '2011-01-05 09:00:00'),
-  ('333', '1000003', '20.00' ,'3333.33', '2011-01-05 09:00:00');
 
-INSERT INTO Include (reservation_number, airline_id, flight_number, leg_number, from_stop_num, dept_date, seat_number, class, meal)  VALUES
-  ('222', 'AA', '111', '1', '1', '2011-01-05 11:00:00', '33F', 'Economy', 'Chips'),
-  ('222', 'AA', '111', '2', '2', '2011-01-05 19:00:00', '33F', 'Economy', 'Chips'),
-  ('111', 'JA', '111', '1', '2', '2011-01-14 03:00:00', '13A', 'First', 'Fish and Chips'),
-  ('333', 'AM', '1337', '1', '1', '2011-01-13 07:00:00', '1A', 'First', 'Sushi');
+INSERT INTO express_way.Employee (ssn, id, is_manager, start_date, hourly_rate, telephone)
+VALUES (111111111, 4, 0, '2010-10-20 08:47:32', 35.00, '655737373');
+INSERT INTO express_way.Employee (ssn, id, is_manager, start_date, hourly_rate, telephone)
+VALUES (111111112, 5, 1, '2010-12-20 08:47:32', 25.00, '655733344');
 
-INSERT INTO Auctions (account_num, reservation_number, airline_id, flight_num, leg_number, class, dept_date, NYOP, is_accepted) VALUES
-  ('1000001', '111', 'AA','111' , '1' , 'First', '2011-01-14 03:00:00' ,'400', TRUE );
 
+INSERT INTO express_way.Reservations (reservation_number, account_number, reservation_date, total_fare, booking_fee, customer_rep_ssn)
+VALUES (444, '1000001', '2011-01-05 09:00:00', 5000.00, 20.00, 111111112);
+INSERT INTO express_way.Reservations (reservation_number, account_number, reservation_date, total_fare, booking_fee, customer_rep_ssn)
+VALUES (333, '1000003', '2011-01-05 09:00:00', 3333.33, 20.00, 111111112);
+INSERT INTO express_way.Reservations (reservation_number, account_number, reservation_date, total_fare, booking_fee, customer_rep_ssn)
+VALUES (111, '1000001', '2011-01-05 09:00:00', 1200.00, 20.00, 111111111);
+INSERT INTO express_way.Reservations (reservation_number, account_number, reservation_date, total_fare, booking_fee, customer_rep_ssn)
+VALUES (222, '1000002', '2011-01-05 09:00:00', 500.00, 20.00, 111111111);
+
+
+
+INSERT INTO express_way.Include (reservation_number, airline_id, flight_number, leg_number, passenger_lname, passenger_fname, dept_date, seat_number, class, meal, from_stop_num)
+VALUES (111, 'JA', 111, 1, NULL, NULL, '2011-01-14', '13A', 'First', 'Fish and Chips', 2);
+INSERT INTO express_way.Include (reservation_number, airline_id, flight_number, leg_number, passenger_lname, passenger_fname, dept_date, seat_number, class, meal, from_stop_num)
+VALUES (222, 'AA', 111, 1, NULL, NULL, '2011-01-05', '33F', 'Economy', 'Chips', 1);
+INSERT INTO express_way.Include (reservation_number, airline_id, flight_number, leg_number, passenger_lname, passenger_fname, dept_date, seat_number, class, meal, from_stop_num)
+VALUES (222, 'AA', 111, 2, NULL, NULL, '2011-01-05', '33F', 'Economy', 'Chips', 2);
+INSERT INTO express_way.Include (reservation_number, airline_id, flight_number, leg_number, passenger_lname, passenger_fname, dept_date, seat_number, class, meal, from_stop_num)
+VALUES (333, 'AM', 1337, 1, NULL, NULL, '2011-01-13', '1A', 'First', 'Sushi', 1);
+
+INSERT INTO Auctions (account_num, reservation_number, airline_id, flight_num, leg_number, class, dept_date, NYOP, is_accepted)
+VALUES
+  ('1000001', '111', 'AA', '111', '1', 'First', '2011-01-14 03:00:00', '400', TRUE);
 
