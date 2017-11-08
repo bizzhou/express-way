e46697112d7c35d3a02f7001fac53f72479d6dee
BZ <545627124@qq.com>
2017-11-08 08:07:50 -0500

    .

---
+++ backend/src/main/java/com/project/expressway/ExpresswayApplication.java
@@ -0,0 +1,12 @@
+package com.project.expressway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpresswayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpresswayApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/project/expressway/ExpresswayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.expressway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpresswayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
