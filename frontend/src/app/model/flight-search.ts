f1db52f01b7b44587ec54bda9fadf2cbe2ff2684
bizzhou <bin.zhou.1@stonybrook.edu>
2017-11-16 15:38:29 -0500

    Add files via upload

---
+++ frontend/src/app/model/flight-search.ts
@@ -0,0 +1,10 @@
+export class FlightSearch{
+    
+    depatureDate: string;
+    returnDate: string;
+    fromAirport: string;
+    toAirport: string;
+    fareType: string;
+    classType: string;
+
+}
