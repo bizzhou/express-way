d28f37383fbb148b1ad7a86edc99416f568732a2
Unknown <bin.zhou.1@stonybrook.edu>
2017-11-17 09:09:32 -0500

    emp signup

---
+++ frontend/src/app/view/employee-signup/employee-signup.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+import { SignUpService } from '../../service/signup.service';
+
+import { Http } from '@angular/http';
+import { Router, ActivatedRoute, RouterStateSnapshot } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-signup',
+  templateUrl: './employee-signup.component.html',
+  styleUrls: ['./employee-signup.component.css'],
+  providers: [SignUpService]
+})
+export class EmployeeSignupComponent {
+
+  constructor(private http: Http, private signUpService: SignUpService, private route: Router) { }
+
+  employee: any = {};
+
+  register () {
+
+    this.signUpService.employeeSignup(this.employee)
+        .then(response => {
+
+          console.log(response);
+
+          if(response == true) {
+            this.route.navigate(['login']);
+          } else {
+            console.log("Input not valid, please user another username/password");
+          }
+        })
+        .catch(error => console.log(error));
+  
+  }
+
+}
---
+++ frontend/src/app/view/user-dialog/dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+})
+export class UserDialogComponent {
+
+  constructor (public dialog: MatDialogRef<UserDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any){
+  }
+
+  update():void{
+    this.dialog.close();
+  }
+
+}
